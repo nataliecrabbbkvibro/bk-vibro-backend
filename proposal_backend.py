46aa8781c74fecb675cc15de15e67b937c03d856
nataliecrabbbkvibro <natalie.crabb@bkvibro.com>
2026-03-10 10:04:27 -0700

    Update proposal_backend.py

--- proposal_backend.py
+++ proposal_backend.py
@@ -259,7 +259,7 @@ def fill_scope_services_table(doc, scope_services):
         row = table.add_row()
 
         if i == 0:
-    set_cell_text(row.cells[0], "Svc")
+    set_cell_text(row.cells[0], "Services")
 else:
     set_cell_text(row.cells[0], "")
 
